7409db1d56eebf16f41985e0d28b1c8f0d708a8e
Hechmi_wael <waelhechmi100@gmail.com>
2025-12-21 13:21:04 +0100

    add Cmake file

---
+++ shellsp/build/CMakeFiles/my_shell.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_shell.dir/shellc.c.o"
+  "CMakeFiles/my_shell.dir/shellc.c.o.d"
+  "my_shell"
+  "my_shell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
